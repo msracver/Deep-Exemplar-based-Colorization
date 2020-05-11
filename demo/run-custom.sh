52bd3ab52b364486d6d08d8be714c79384fcc86d
Seungwon Jeong <jsw1295@gmail.com>
2020-05-11 11:42:03 +0900

    Update run-custom.sh

--- demo/run-custom.sh
+++ demo/run-custom.sh
@@ -4,6 +4,8 @@ mkdir -p /src/app/custom-example/res
 mkdir -p /src/app/custom-example/flow
 mkdir -p /src/app/custom-example/combo_new
 
+bin/deep_image_analogy models/deep_image_analogy/ /src/app/custom-example/ 0 2 0
+
 bin/similarity_combo models/similarity_subnet/ /src/app/custom-example/ 0 2 0
 
 python3 ../colorization_subnet/test.py --short_size 256 --test_model models/colorization_subnet/example_net.pth --data_root /src/app/custom-example/ --out_dir /src/app/custom-example/res/ --gpu_id 0
